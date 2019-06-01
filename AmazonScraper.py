d1a990c4314772eabf7713b9c6cd753772e12098
Rohit Neppalli <neppalli.rohit@gmail.com>
2019-06-01 16:54:45 -0500

    Better url retrieval - at double the speed

--- AmazonScraper.py
+++ AmazonScraper.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from googlesearch import search
 import csv
 import requests
@@ -11,25 +10,26 @@ def scrape_multiple(search_terms, delay=1):
         scrape(search_term, delay=delay, suppress_labels=(index!=0))
 
 def scrape(search_term, delay=0, suppress_labels=False):
-    url = get_url(search_term)
+    start_time = time.time()
+    url = get_url_v2(search_term)
+    print("Url time: {}".format(time.time() - start_time))
     headers = {'User-Agent': 'Mozilla/{}.0 (Windows NT 6.1) {} Chrome/{}.0.2228.0 Safari/537.36'.format(
         random.randint(1, 6), random.choice(["", "AppleWebKit/537.36 (KHTML, like Gecko)"]), random.randint(1, 41))}
     request = requests.get(url, headers=headers)
     doc = html.fromstring(request.content)
     try:
-        title = doc.xpath(
-            "//h1[@id='title']//text()")[1].replace("\n", "").strip()
-        author = doc.xpath(
-            "//a[@class='a-link-normal contributorNameID']//text()")[0]
+        title = doc.xpath("//h1[@id='title']//text()")[1].replace("\n", "").strip()
+        author = doc.xpath("//a[@class='a-link-normal contributorNameID']//text()")[0]
         reviews = doc.xpath("//span[@id='acrCustomerReviewText']//text()")[0]
         reviews = reviews[:reviews.find(" ")].replace(",", "")
-        rating = doc.xpath(
-            "//span[@class='arp-rating-out-of-text a-color-base']//text()")[0]
+        rating = doc.xpath("//span[@class='arp-rating-out-of-text a-color-base']//text()")[0]
         rating = rating[:rating.find(" ")]
         category = doc.xpath("//span[@class='cat-link']//text()")
 
-        stuff = doc.xpath("//td[@class='bucket']//div[@class='content']//li//text()")
-        stuff = [elem.replace("\n", "").strip() for elem in stuff if elem.replace("\n", "").strip() != "" and "{" not in elem and len(elem) > 3]
+        # stuff = doc.xpath("//td[@class='bucket']//div[@class='content']//li//text()")
+        # stuff = [elem.replace("\n", "").strip() for elem in stuff if elem.replace("\n", "").strip() != "" and "{" not in elem and len(elem) > 3]
+
+        #summary = doc.xpath("//div[@id='iframeContent']")
 
         if not suppress_labels:
             print(["Url", "Title", "Author", "Reviews", "Rating", "Category"])
@@ -37,12 +37,20 @@ def scrape(search_term, delay=0, suppress_labels=False):
 
     except:
         print("Error")
-        return False
+    print("Total time: {}".format(time.time() - start_time))
     time.sleep(delay)
-    return True
 
 def get_url(search_term):
     results = search(search_term + " amazon", stop=1)
     for result in results:
         if "amazon.com" in result:
             return result
+
+def get_url_v2(search_term):
+    amazon_url = "https://www.amazon.com/s?k={}&ref=nb_sb_noss_2".format(search_term.replace(" ", "+"))
+    headers = {'User-Agent': 'Mozilla/6.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0'}
+    request = requests.get(amazon_url, headers=headers)
+    doc = html.fromstring(request.content)
+
+    url = "https://www.amazon.com" + doc.xpath("//a[@class='a-link-normal']")[0].attrib['href']
+    return url
--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
 from AmazonScraper import scrape, scrape_multiple
 
-scrape_multiple(["Percy Jackson and the Lightning Thief", "Harry Potter and the Prisoner of Azkaban"], delay=1)
+scrape_multiple(["the martian andy weir"], delay=0)
